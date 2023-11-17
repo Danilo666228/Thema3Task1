544192c498c264ad3dc6988de6655f6b8f210bfa
Danya Kovinskiy <129722708+Danilo666228@users.noreply.github.com>
2023-11-17 21:40:06 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,49 @@
+import java.util.*
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    val random = Random()
+
+    println("Игра 'Камень-Ножницы-Бумага'")
+
+    while (true) {
+        // Генерация выбора компьютера
+        val computerChoice = random.nextInt(3) + 1
+
+        // Запрос выбора игрока
+        println("Ваш выбор:")
+        println("1. Камень")
+        println("2. Ножницы")
+        println("3. Бумага")
+
+        val playerChoice = scanner.nextInt()
+        scanner.nextLine()
+
+        println()
+
+        // Вывод выбора компьютера
+        println("Выбор компьютера:")
+        when (computerChoice) {
+            1 -> println("Камень")
+            2 -> println("Ножницы")
+            3 -> println("Бумага")
+        }
+
+        println()
+
+        // Определение победителя
+        if (playerChoice == computerChoice) {
+            println("Ничья! Игра переигрывается.")
+        } else if ((playerChoice == 1 && computerChoice == 2) ||
+            (playerChoice == 2 && computerChoice == 3) ||
+            (playerChoice == 3 && computerChoice == 1)
+        ) {
+            println("Вы выиграли!")
+        } else {
+            println("Компьютер выиграл!")
+        }
+
+        println()
+
+    }
+}
